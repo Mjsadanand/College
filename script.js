1eeb5e96cbb5bebb96a093fbf868f5b952b84da1
Sadanand J M <147255461+Mjsadanand@users.noreply.github.com>
2023-10-07 20:27:30 +0530

    ADDED

---
+++ script.js
@@ -0,0 +1,12 @@
+function getname(){
+    Name=prompt("Enter Your Sweet Name");
+    alert("Welcome to Mr/Mrs: " +Name);
+}
+/*var today=new Date();
+var h=today.getHours();
+var m=today.getMinutes();
+var s=today.getSeconds();
+var ms=today.getMilliseconds
+document.getElementById('txt').innerHTML=h+":"+m+":"+s;
+var dt=Date();
+document.write("Date and Time:"+date);*/
